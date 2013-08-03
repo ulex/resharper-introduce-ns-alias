63f4cea5e04c338f2e962ff0c945dde068e68a93
unknown <Administrator@turi.estonia.local>
2013-08-03 03:16:59 -0700

    add publish script

---
+++ ReSharperIntroduceNsAlias8.0/Publish.ps1
@@ -0,0 +1,5 @@
+﻿$package = (ls .\bin\Release\*.nupkg | sort LastWriteTIme | select -first 1).FullName
+
+#nuget setApiKey <key guid> -Source https://resharper-plugins.jetbrains.com 
+
+nuget push "$package" -Source https://resharper-plugins.jetbrains.com
