8aa09e63ca9eb47ca77ac642aa07b660f9c65189
unknown <Administrator@turi.estonia.local>
2013-08-03 03:04:20 -0700

    Fixed typo, add icon, add project reference, configure auto build

--- ReSharperIntroduceNsAlias8.0/Properties/AssemblyInfo.cs
+++ ReSharperIntroduceNsAlias8.0/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 using System.Reflection;
-using JetBrains.ActionManagement;
 using JetBrains.Application.PluginSupport;
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+using JetBrains.ProjectModel.model2.Assemblies;
+
 [assembly: AssemblyTitle("IntroduceNsAlias")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Introduce C# namespace alias refactoring plugin for ReSharper")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("IntroduceNsAlias")]
@@ -14,9 +14,8 @@ using JetBrains.Application.PluginSupport;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyFileVersion("1.0.5.*")]
 // The following information is displayed by ReSharper in the Plugins dialog
 [assembly: PluginTitle("Introduce Namespace Alias")]
 [assembly: PluginDescription("")]
---
+++ ReSharperIntroduceNsAlias8.0/buildNuPack.ps1
@@ -0,0 +1,16 @@
+﻿pushd $args[0]
+
+$nuspec = ".\IntroduceNsAlias.nuspec"
+$version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo((gi .\bin\Release\IntroduceNsAlias.dll).FullName)
+$xml = [xml] (gc $nuspec)
+$node = $xml.SelectSingleNode("package/metadata/version").InnerText = $version.ProductVersion
+
+
+$xml.OuterXml > out.nuspec
+
+nuget.exe pack out.nuspec
+
+Move-Item *.nupkg .\bin\Release
+ri .\out.nuspec
+
+popd
